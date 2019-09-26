4b8ba110f27cdc59f6959f266df4a339d8703633
Elias Roussos <eliasisaiah@outlook.com>
2019-09-26 13:08:20 -0400

    -basic data validation implemented for survey

--- app/public/frontEnd.js
+++ app/public/frontEnd.js
@@ -1,17 +1,23 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
     $('select').formSelect();
 
-    console.log($("button"));
-    $("body").on("submit", function(event) {
-        event.preventDefault();
+    // console.log($("button"));
+    $("button").on("click", function (event) {
+        console.log("clicked!");
 
         let answers = [];
 
-        let name = $("#name").val();
-        
-        for( let i = 1; i < 11; i++) {
+        const name = $("#name").val();
+        if(name === "") {
+            alert("name cannot be empty");
+            event.preventDefault();
+            return;
+        }
+        const picURL = $("#pictureURL").val();
+
+        for (let i = 1; i < 11; i++) {
             console.log($(`#question${i}`).val());
             answers.push($(`#question${i}`).val());
         }
@@ -20,29 +26,29 @@ $( document ).ready(function() {
 
 
         console.log("submitted!");
-        $.post('/api', $('#theForm').serialize())
+        $.post('/api', { "testKey": "teststring" }).then(data => console.log(data));
     })
 
     let formObj = {
-        testKey : "testString"
+        testKey: "testString"
     }
-/*     $("a.test").on("click", function(event) {
-
-        console.log("reached testButton handler");
-        console.log($('#theForm').serialize());
-
-        let mondays = $("#question1").val();
-        console.log("mondays", mondays);
-
-
-        $('select').formSelect('getSelectedValues');
-
-        testObj = {
-            testKey: "testString"
-        }
-
-        $.post('/api', testObj, function (data) {
-            console.log(data);
-        })
-    }) */
+    /*     $("a.test").on("click", function(event) {
+    
+            console.log("reached testButton handler");
+            console.log($('#theForm').serialize());
+    
+            let mondays = $("#question1").val();
+            console.log("mondays", mondays);
+    
+    
+            $('select').formSelect('getSelectedValues');
+    
+            testObj = {
+                testKey: "testString"
+            }
+    
+            $.post('/api', testObj, function (data) {
+                console.log(data);
+            })
+        }) */
 });
