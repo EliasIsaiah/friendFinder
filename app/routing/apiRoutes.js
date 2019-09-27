b58b7fec6d550fac3513100e69a3cad2212180b9
Elias Roussos <eliasisaiah@outlook.com>
2019-09-26 21:57:00 -0400

    -added a comment and a semi in index.js and apiRoutes.js respectively

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 const friends = require("../data/friends.js")
 
-routes.post('/', (req, res) => {
+routes.post('/', (req, res) => { //localhost:3000/api
   res.status(200).json({ message: 'apiRoutes Connected!' });
   console.log(req.body);
 });
--- app/routing/index.js
+++ app/routing/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./apiRoutes.js');
 const htmlRoutes = require('./htmlRoutes.js');
 
 routes.use('/api', apiRoutes);
-routes.use('/', htmlRoutes)
+routes.use('/', htmlRoutes);
 
 // routes.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Connected!' });
